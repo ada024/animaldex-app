1d28a28cf94610e12b6ffa8e0311687854fed57e
ada024 <andreasm.dale@bkkfiber.no>
2021-03-15 13:59:27 +0100

    Initial Commit

---
+++ AnimalDex App/AnimalDex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalDex_AppApp.swift
+//  AnimalDex App
+//
+//  Created by Drowiz on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimalDex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
